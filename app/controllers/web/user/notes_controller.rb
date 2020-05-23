59e03fa8bbb0cb03435f7246e20cc9e9196836c9
Alexander Pashchenko <point212@gmail.com>
2020-05-23 21:09:03 +0300

    Notes controller logic simple index.haml view

--- app/controllers/web/user/notes_controller.rb
+++ app/controllers/web/user/notes_controller.rb
@@ -1,5 +1,6 @@
 class Web::User::NotesController < Web::User::ApplicationController
   def index
-
+    notes = current_user.notes
+    render :index, locals: { notes: notes }, status: :ok
   end
 end
