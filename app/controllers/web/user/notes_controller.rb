43cc7e305686a66dc07044fa6bc71742a72aaecb
Alexander Pashchenko <point212@gmail.com>
2020-05-23 19:01:11 +0300

    Satisfy test for Notes controller

---
+++ app/controllers/web/user/notes_controller.rb
@@ -0,0 +1,5 @@
+class Web::User::NotesController < Web::User::ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :web do
+    namespace :user do
+      resources :notes, only: %[index]
+    end
+  end
 end
